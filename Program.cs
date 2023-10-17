e55fa6851300f4b3e2cbe2e965c41d79a4f7eb3e
Monsterter <ibrahimsenc@gmail.com>
2023-10-17 13:06:53 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TryCatch
+{
+public class Program
+{
+   
+    
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Bir sayı giriniz..");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı :" + sayi );
+
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Hata : **" + ex.Message.ToString());
+        }
+
+        finally // Opsinional block..
+        {
+            Console.WriteLine("İşlem tamamlandı*********");
+        }
+
+        Console.WriteLine("***Bu kod bloğu başarılı bir şekilde çalışmalı***");
+
+
+        try
+        {
+            int a = int.Parse(null);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("Boş değer giridinz.."); // TODO
+            Console.WriteLine(ex);
+        }
+
+    }
+    }
+}
